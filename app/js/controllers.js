ac57d6433455e15c116a254220d97e39786cd755
Fernando Ping <nandinho.lap1@yahoo.com>
2017-10-19 09:23:29 -0200

    ta foda em

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', []);
+var app = angular.module('app');
 app.controller('CategoriasController',function($scope, $http) {
     $scope.categorias = ['um','chance','batata'];
-}
+});
